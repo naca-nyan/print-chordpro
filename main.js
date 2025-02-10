4a18940c07d166c303c677da0e8869d53766429d
naca-nyan <naca.nyan@gmail.com>
2025-02-11 03:28:07 +0900

    Add site

---
+++ main.js
@@ -0,0 +1,26 @@
+import ChordPro from "https://cdn.jsdelivr.net/npm/chordproject-parser@0.2.14/+esm";
+function render(s, target) {
+  const parser = new ChordPro.ChordProParser();
+  const song = parser.parse(s);
+  document.title = song.title;
+  const formatter = new ChordPro.HtmlFormatter();
+  const htmlTags = formatter.format(song);
+  target.innerHTML = htmlTags.join("\n");
+}
+const $ = (s) => document.querySelector(s);
+const target = $("main");
+const saved = localStorage.getItem("chordpro");
+if (saved) {
+  $("textarea").value = saved;
+  render(saved, target);
+}
+$("textarea").onchange = (e) => {
+  const s = e.target.value;
+  if (s) {
+    render(s, target);
+  }
+  localStorage.setItem("chordpro", s);
+};
+$("button").onclick = () => {
+  print();
+};
